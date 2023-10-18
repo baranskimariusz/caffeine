1b81a07901dc3c6854030b5205b2c3f047c3cd9b
baranskimariusz <mariusz.baranski@icloud.com>
2023-10-18 22:45:24 +0200

    Updated functions and created data.py

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 import datetime
 import math
-import flask
+from data import user_data, drinks
 
-#variables
-age = 22 #int(input("Please write your age: "))
-weight = 82 #int(input("Please write your weight: "))
-gender = "male" #str(input("Please write your gender (male/female): "))
+def get_user_data():
+  user_data["age"] = int(input("Please write your age: "))
+  user_data["weight"] = int(input("Please write your weight: "))
+  user_data["gender"] = input("Please write your gender (male/female): ")
 
 def time_diff():
   time = (datetime.datetime.now())
   time_string = time.strftime('%H:%M')
   return time_string
 
-time_of_intake = round(float(input("Please write your time of intake (eg. 12.30): ")))
-real_time = round(float(time_diff().replace(':', '.')))
-t = real_time - time_of_intake
+def time_since():
+  time_of_intake = round(float(input("Please write your time of intake (eg. 12.30): ")))
+  real_time = round(float(time_diff().replace(':', '.')))
+  t = real_time - time_of_intake
+  return t
 
-#dictionary with a caffeine concentration multiplier
-drinks = {
-  "Caffee Latte": 0.404,
-  "Cappuccino": 0.268,
-  "Flat White": 0.45,
-  "Espresso": 2.1,
-  "Energy Drink": 0.32,
-  "Dzik Energy Drink": 0.4
-}
-
-#algorithm
 def algorithm():
   coffee = input("Please write type of the drink consumed: ")
   quantity = float(input("Please write consumed amount in ml: "))
@@ -35,7 +26,9 @@ def algorithm():
   d = (drinks.get(user_choice) * quantity) #caffeine in a chosen drink
 
   c = math.log(0.9) #coefficient
-  CI = d * pow(t, c)
-  print(CI)
+  CI = d * pow(time_since(), c)
+  print("Your current caffeine concentration is", round(CI), "ml.")
 
+get_user_data()
+print(user_data)
 algorithm()
---
+++ data.py
@@ -0,0 +1,14 @@
+user_data = {
+  "age" : None,
+  "weight": None,
+  "gender": None
+}
+
+drinks = {
+  "Caffee Latte": 0.404,
+  "Cappuccino": 0.268,
+  "Flat White": 0.45,
+  "Espresso": 2.1,
+  "Energy Drink": 0.32,
+  "Dzik Energy Drink": 0.4
+}
